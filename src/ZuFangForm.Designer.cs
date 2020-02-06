ded71145db737cf3fb774a03abbb4e8656a4329a
Fan Hongtao <fanhongtao@gmail.com>
2020-02-06 14:21:55 +0800

    添加简单的组方功能

--- src/ZuFangForm.Designer.cs
+++ src/ZuFangForm.Designer.cs
@@ -14,6 +14,11 @@ namespace ZhongYi
 		/// Designer variable used to keep track of non-visual components.
 		/// </summary>
 		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Panel headPanel;
+		private ZhongYi.Filter.NameFilter nameFilter;
+		private System.Windows.Forms.Button addButton;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.RichTextBox outputText;
 		
 		/// <summary>
 		/// Disposes resources used by the form.
@@ -36,12 +41,75 @@ namespace ZhongYi
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.headPanel = new System.Windows.Forms.Panel();
+			this.nameFilter = new ZhongYi.Filter.NameFilter();
+			this.addButton = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.outputText = new System.Windows.Forms.RichTextBox();
+			this.headPanel.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// headPanel
+			// 
+			this.headPanel.Controls.Add(this.nameFilter);
+			this.headPanel.Controls.Add(this.addButton);
+			this.headPanel.Controls.Add(this.label1);
+			this.headPanel.Dock = System.Windows.Forms.DockStyle.Top;
+			this.headPanel.Location = new System.Drawing.Point(0, 0);
+			this.headPanel.Name = "headPanel";
+			this.headPanel.Size = new System.Drawing.Size(762, 28);
+			this.headPanel.TabIndex = 0;
+			// 
+			// nameFilter
+			// 
+			this.nameFilter.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.nameFilter.Hint = "输入中药名 或 拼音（全拼 或 缩写）";
+			this.nameFilter.Location = new System.Drawing.Point(61, 0);
+			this.nameFilter.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.nameFilter.Name = "nameFilter";
+			this.nameFilter.Size = new System.Drawing.Size(626, 28);
+			this.nameFilter.TabIndex = 2;
+			// 
+			// addButton
+			// 
+			this.addButton.Dock = System.Windows.Forms.DockStyle.Right;
+			this.addButton.Location = new System.Drawing.Point(687, 0);
+			this.addButton.Name = "addButton";
+			this.addButton.Size = new System.Drawing.Size(75, 28);
+			this.addButton.TabIndex = 1;
+			this.addButton.Text = "添加";
+			this.addButton.UseVisualStyleBackColor = true;
+			// 
+			// label1
+			// 
+			this.label1.Dock = System.Windows.Forms.DockStyle.Left;
+			this.label1.Location = new System.Drawing.Point(0, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(61, 28);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "中药";
+			// 
+			// outputText
+			// 
+			this.outputText.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.outputText.Location = new System.Drawing.Point(0, 28);
+			this.outputText.Name = "outputText";
+			this.outputText.Size = new System.Drawing.Size(762, 429);
+			this.outputText.TabIndex = 1;
+			this.outputText.Text = "";
 			// 
 			// ZuFangForm
 			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Text = "ZuFangForm";
+			this.ClientSize = new System.Drawing.Size(762, 457);
+			this.Controls.Add(this.outputText);
+			this.Controls.Add(this.headPanel);
 			this.Name = "ZuFangForm";
+			this.Text = "ZuFangForm";
+			this.headPanel.ResumeLayout(false);
+			this.ResumeLayout(false);
+
 		}
 	}
 }
--- src/ZuFangForm.cs
+++ src/ZuFangForm.cs
@@ -17,6 +17,9 @@ namespace ZhongYi
 	/// </summary>
 	public partial class ZuFangForm : Form
 	{
+		private const int TITLE_SIZE = 30;
+		private const int TEXT_SIZE = 20;
+		
 		public ZuFangForm()
 		{
 			//
@@ -27,6 +30,36 @@ namespace ZhongYi
 			//
 			// TODO: Add constructor code after the InitializeComponent() call.
 			//
+			nameFilter.queryNames = Data.Instance.queryZhongyaoName;
+			nameFilter.NameSelected += NameSelected;
+		}
+		
+		void NameSelected(object sender, Filter.NameEventArgs e)
+		{
+			addZhongYao(e.NamnInfo.Name);
+		}
+		
+		/// <summary>
+		/// 添加一味中药
+		/// </summary>
+		/// <param name="name"></param>
+		private void addZhongYao(string name)
+		{
+			ZhongYaoInfo info = Data.Instance.getZhongYao(name);
+			if (info == null) {
+				Logger.error("没有收录 [" + name + "]");
+			} else {
+				show(info.name, TEXT_SIZE);
+				outputText.AppendText("\n");
+			}
+		}
+		
+		private void show(string text, int size)
+		{
+			int start = outputText.Text.Length;
+			outputText.AppendText(text);
+			outputText.Select(start, outputText.Text.Length - start);
+			outputText.SelectionFont = new Font("宋体", size); 
 		}
 	}
 }
