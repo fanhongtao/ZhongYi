75d85cb61fafb7418ddbbb63f9db3eec47437f94
Fan Hongtao <fanhongtao@gmail.com>
2020-02-07 17:09:40 +0800

    直接读取xml文件内容返回

--- web/app/views.py
+++ web/app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template, flash, redirect, request
 from app import app
+from pathlib import Path
 
 @app.route('/')
 @app.route('/index')
@@ -12,4 +13,13 @@ def zhongyaochaxun():
     name = request.args.get('yao')
     if (name == None):
         return render_template('zhongyaochaxun.html')
-    return render_template('zhongyaochaxun.html', zhongyao=name)
+    file = Path("../data/zhongyao/" + name + ".xml")
+    if file.is_file():
+        content = read_file(file)
+        return render_template('zhongyaochaxun.html', zhongyao=content)
+    return render_template('zhongyaochaxun.html', zhongyao="没有收录：" + name)
+
+def read_file(file_path):
+    with open(file_path,'r',encoding='utf-8') as f:
+        data = f.read()
+    return data
