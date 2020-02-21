926462b02ffc456a8773a6b40c5018848d07b493
Fan Hongtao <fanhongtao@gmail.com>
2020-02-21 11:01:06 +0800

    1. 中药介绍中，将所涉及的中药使用 <a></a> 标签标出，方便在浏览器中进行跳转 2. 将书名中的 "·" 替换成 "•" （在浏览器中显示更清楚） 3. 参考我 blog 中的风格修改 CSS

--- web/app/static/js/zhongyao.js
+++ web/app/static/js/zhongyao.js
@@ -30,14 +30,14 @@ function queryZhongYao(zhongyao) {
 function parseXML(xml) {
   var name = $(xml).find("名称")[0];
   var content = "<h1>" + $(name).attr('hz') + " ( " + $(name).attr('py') + " )</h1>";
-  content += getElement(xml, "介绍");
+  content += getHtmlElement(xml, "介绍");
   content += getYaoXing(xml);
-  content += getElement(xml, "功效");
+  content += getHtmlElement(xml, "功效");
   content += getYingYong(xml);
-  content += getElement(xml, "用法用量");
-  content += getElement(xml, "使用注意");
+  content += getHtmlElement(xml, "用法用量");
+  content += getHtmlElement(xml, "使用注意");
   content += getJianBieYongYao(xml);
-  content += getElement(xml, "其他");
+  content += getHtmlElement(xml, "其他");
   content += getFuYao(xml);
   $("#result").html(content);
   $("#result a:not([herf])").each(function() {
@@ -45,13 +45,13 @@ function parseXML(xml) {
   });
 }
 
-function getElement(xml, name) {
+function getHtmlElement(xml, name) {
   var ele = $(xml).find(name);
   if (ele.length == 0) {
     content = "";
   } else {
     content = "<h2>" + name + "</h2>";
-    content += $(ele[0]).text();
+    content += $(ele[0]).html();
   }
   return content;
 }
@@ -82,8 +82,7 @@ function getYingYong(xml) {
   $(yingyong).find("para").each(function() {
     var title = $($(this).find("title")[0]).text();
     content += "<p><font class='bold'>" + title + "</font><br/>";
-    $(this).find("title").remove();
-    content += $(this).html() + "</p>";
+    content += $($(this).find("text")[0]).html() + "</p>";
   });
   return content;
 }
@@ -93,19 +92,23 @@ function getJianBieYongYao(xml) {
   if (jianbie.length == 0) {
     return "";
   }
-  var count = 0;
+  
   var content = "<h2>" + "鉴别用药" + "</h2>";
-  content += "<font class='bold'>";
-  $(jianbie[0]).find("鉴药").each(function() {
-    count++;
-    if (count > 1) {
-      content += ", ";
-    }
-    content += getLink($(this).text());
-    $(this).remove();
+  jianbie.each(function() {
+    var count = 0;
+    content += "<font class='bold'>";
+    $(this).find("鉴药").each(function() {
+      count++;
+      if (count > 1) {
+        content += ", ";
+      }
+      content += getLink($(this).text());
+      $(this).remove();
+    });
+    content += "</font><br/>";
+    content += $(this).text();
+    content += "<br/>";
   });
-  content += "</font><br/>";
-  content += $(jianbie[0]).text();
   return content;
 }
 
